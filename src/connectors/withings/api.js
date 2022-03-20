6d1801bb18c0109596789703d2ad4b228c7082ab
paultranvan <paul@cozycloud.cc>
2022-03-20 21:22:16 +0100

    Add API endpoints

--- src/connectors/withings/api.js
+++ src/connectors/withings/api.js
@@ -1,13 +1,22 @@
 const axios = require('axios').default
 
- //axios.defaults.headers.common['Authorization'] = `Bearer 0d46f638f0632909fa4a9b27660f4209b23558a0`;
+// TODO: pagination
+
+const convertDateInTimestamp = (date) => {
+    return Math.floor(date.getTime() / 1000)
+}
+
+const convertDateInYMD = (date) => {
+    return date.toISOString().split('T')[0]
+}
+
 
 const getSleepSummary = async (token, startDate, endDate) => {
     const url = 'https://wbsapi.withings.net/v2/sleep'
     const params = {
         action: 'getsummary',
-        startdateymd: startDate,
-        enddateymd: endDate,
+        startdateymd: convertDateInYMD(startDate),
+        enddateymd: convertDateInYMD(endDate),
     }
     const resp = await axios.get(url, {
         params,
@@ -16,7 +25,84 @@ const getSleepSummary = async (token, startDate, endDate) => {
     return resp.data
 }
 
+const getMeasure = async (token, startDate, endDate) => {
+    const url = 'https://wbsapi.withings.net/measure'
+
+    const params = {
+        action: 'getmeas',
+        meastypes: '1,4,6,8,11,76,77,88', // TODO: use const with measure name
+        category: 1,
+        startdate: convertDateInTimestamp(startDate),
+        enddate: convertDateInTimestamp(endDate)
+    }
+    const resp = await axios.get(url, {
+        params,
+        headers: { Authorization: `Bearer ${token}` }
+    })
+    return resp.data
+}
+
+const getActivity = async (token, startDate, endDate) => {
+    const url = 'https://wbsapi.withings.net/v2/measure'
+    const params = {
+        action: 'getactivity',
+        startdateymd: convertDateInYMD(startDate),
+        enddateymd: convertDateInYMD(endDate),
+        data_fields: 'steps,distance,elevation,calories,hr_average' 
+    }
+    const resp = await axios.post(url, params, {
+        headers: { Authorization: `Bearer ${token}`}
+    })
+    return resp.data
+}
+
+const getHighFrequencyActivity = async (token, startDate, endDate) => {
+    const url = 'https://wbsapi.withings.net/v2/measure'
+    const params = {
+        action: 'getintradayactivity',
+        startdate: convertDateInTimestamp(startDate),
+        enddate: convertDateInTimestamp(endDate),
+        data_fields: 'steps,distance,calories,heart_rate' 
+    }
+    const resp = await axios.post(url, params, {
+        headers: { Authorization: `Bearer ${token}`}
+    })
+    return resp.data
+}
+
+const getWorkouts = async (token, startDate, endDate) => {
+    const url = 'https://wbsapi.withings.net/v2/measure'
+    const params = {
+        action: 'getworkouts',
+        startdate: convertDateInTimestamp(startDate),
+        enddate: convertDateInTimestamp(endDate),
+        data_fields: 'steps,distance,calories,hr_average' 
+    }
+    const resp = await axios.post(url, params, {
+        headers: { Authorization: `Bearer ${token}`}
+    })
+    return resp.data
+}
+
+
+const getHeartList = async (token, startDate, endDate) => {
+    const url = 'https://wbsapi.withings.net/v2/heart'
+    const params = {
+        action: 'list',
+        startdate: convertDateInTimestamp(startDate),
+        enddate: convertDateInTimestamp(endDate)
+    }
+    const resp = await axios.post(url, params, {
+        headers: { Authorization: `Bearer ${token}`}
+    })
+    return resp.data
+}
 
 module.exports = {
-    getSleepSummary
+    getSleepSummary,
+    getMeasure,
+    getActivity,
+    getHighFrequencyActivity,
+    getWorkouts,
+    getHeartList
 }
--- src/connectors/withings/index.js
+++ src/connectors/withings/index.js
@@ -2,36 +2,165 @@ const fs = require('fs')
 const api = require('./api')
 const { Oauth } = require('./oauth')
 
-const getSleepData = async () => {
-  try {
-    const token = JSON.parse(fs.readFileSync('.token.json')) 
-    if (!token || !token.accessToken) {
-      throw new Error('Empty file')
-    } 
-    const accessToken = token.accessToken
-    let resp = await api.getSleepSummary(accessToken, '2021-01-01', '2022-03-07')
-
-    if (resp.status === 401) {
-      oauth = new Oauth()
-      const data = await oauth.refreshAccessToken(token.refreshToken)
-      oauth.writeTokenFile(data)
-      resp = await api.getSleepSummary(data.accessToken, '2021-02-01', '2022-03-07')
-    }
-
-    fs.writeFileSync('data/sleep.json', JSON.stringify(resp.body, null, 2))
-    console.log('File saved')
-
+const handleError = (error) => {
+  if ((error.code && error.code === 'ENOENT') || (error.message && error.message.match(/Empty file/))) {
+    console.log('No token found: please run `npm run get-token`')
+    return
+  } else {
+    console.error(error)
   }
-  catch (error) {
-    if (error.code === 'ENOENT' || error.message.match(/Empty file/)) {
-      console.log('No token found: please run `npm run get-token`')
-      return
-    } else {
-      console.error(error)
-    }
-  }
-
-
 }
 
-getSleepData()
+const getAccessToken = async () => {
+  let token = JSON.parse(fs.readFileSync('.token.json')) 
+  if (!token || !token.accessToken) {
+    throw new Error('Empty file')
+  }
+  const resp = await api.getSleepSummary(token.accessToken, new Date(), new Date())
+  if (resp.status === 401) {
+    console.log('Need to refresh token')
+    oauth = new Oauth()
+    token = await oauth.refreshAccessToken(token.refreshToken)
+    oauth.writeTokenFile(token)
+  }
+  if (resp.status > 0) {
+    throw 'Error while trying to get token'
+  }
+  return token.accessToken
+}
+
+const saveFile = (fileName, data) => {
+  const path = `data/${fileName}`
+  fs.writeFileSync(path, JSON.stringify(data, null, 2))
+  console.log(`${path} saved`)
+}
+
+const getSleepData = async (token) => {
+  try {
+    console.log('Get sleep data...')
+
+    const startDate = new Date('2021-01-01')
+    const endDate = new Date()
+
+    const resp = await api.getSleepSummary(token, startDate, endDate)
+
+    if (resp.status > 0) {
+      // See https://developer.withings.com/api-reference/#section/Response-status 
+      return console.error(resp)
+    }
+    saveFile('sleep.json', resp.body)
+  }
+  catch (error) {
+    return handleError(error)
+  }
+}
+
+const getMeasureData = async (token) => {
+  try {
+    console.log('Get measure data...')
+
+    const startDate = new Date('2021-01-01')
+    const endDate = new Date()
+
+    const resp = await api.getMeasure(token, startDate, endDate)
+
+    if (resp.status > 0) {
+      // See https://developer.withings.com/api-reference/#section/Response-status 
+      return console.error(resp)
+    }
+    saveFile('measure.json', resp.body)
+  }
+  catch (error) {
+    return handleError(error)
+  }
+}
+
+const getActivityData = async (token) => {
+  try {
+    console.log('Get activity data...')
+
+    const startDate = new Date('2021-01-01')
+    const endDate = new Date()
+
+    const resp = await api.getActivity(token, startDate, endDate)
+
+    if (resp.status > 0) {
+      // See https://developer.withings.com/api-reference/#section/Response-status 
+      return console.error(resp)
+    }
+    saveFile('activity.json', resp.body)
+  }
+  catch (error) {
+    return handleError(error)
+  }
+}
+
+const getHighFrequencyData = async (token) => {
+  try {
+    console.log('Get heart data...')
+
+    const startDate = new Date('2021-01-01')
+    const endDate = new Date()
+    const resp = await api.getHighFrequencyActivity(token, startDate, endDate)
+
+    if (resp.status > 0) {
+      // See https://developer.withings.com/api-reference/#section/Response-status 
+      return console.error(resp)
+    }
+    saveFile('highactivity.json', resp.body)
+  }
+  catch (error) {
+    return handleError(error)
+  }
+}
+
+const getWorkoutData = async (token) => {
+  try {
+    console.log('Get workout data...')
+
+    const startDate = new Date('2021-01-01')
+    const endDate = new Date()
+    const resp = await api.getWorkouts(token, startDate, endDate)
+
+    if (resp.status > 0) {
+      // See https://developer.withings.com/api-reference/#section/Response-status 
+      return console.error(resp)
+    }
+    saveFile('workout.json', resp.body)
+  }
+  catch (error) {
+    return handleError(error)
+  }
+}
+
+
+const getHeartData = async (token) => {
+  try {
+    console.log('Get heart data...')
+
+    const startDate = new Date('2021-01-01')
+    const endDate = new Date()
+    const resp = await api.getHeartList(token, startDate, endDate)
+
+    if (resp.status > 0) {
+      // See https://developer.withings.com/api-reference/#section/Response-status 
+      return console.error(resp)
+    }
+    saveFile('heart.json', resp.body)
+  }
+  catch (error) {
+    return handleError(error)
+  }
+}
+
+const main = async () => {
+  const token = await getAccessToken()
+  getSleepData(token)
+  getMeasureData(token)
+  getActivityData(token)
+  getHighFrequencyData(token)
+  getWorkoutData(token)
+  getHeartData(token)
+}
+main()
+
