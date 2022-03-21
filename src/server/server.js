f8a6c4afb6465c70e5fcb0aba505661b203a575f
paultranvan <paul@cozycloud.cc>
2022-03-21 02:11:47 +0100

    Server is generic on series

--- src/server/server.js
+++ src/server/server.js
@@ -14,16 +14,18 @@ app.use(cors({origin: '*'}));
 app.get('/data/:type', (req, res) => {
     try {
       const dataType = req.params.type
-      const startDate = req.query.startDate
-      const endDate = req.query.endDate
+      const startDate = new Date(req.query.startDate)
+      const endDate = new Date(req.query.endDate)
       console.log(`received request for ${dataType} from ${startDate} to ${endDate}`)
 
       const dataPath = `data/${dataType}.json`
       const data = JSON.parse(fs.readFileSync(dataPath))
-      const series = data.series
 
-      const filteredSeries = series.filter(serie => serie.date >= startDate && serie.date <= endDate)
-      res.send(filteredSeries)
+      const filteredData = data.series.filter(serie => {
+        const dateSerie = typeof serie.date === 'number' ? new Date(serie.date * 1000) : new Date(serie.date)
+        return dateSerie >= startDate && dateSerie <= endDate
+      })
+      res.send(filteredData)
 
     } catch (error) {
         console.error(error)
