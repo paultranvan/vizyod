984b0490793f6bf0405acc8c8c9866182ad23d55
Paul Tran-Van <paul@cozycloud.cc>
2022-04-18 01:36:05 +0200

    feat: Use browser locale for date picker

--- src/components/DateSelector.jsx
+++ src/components/DateSelector.jsx
@@ -3,15 +3,25 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import TextField from '@mui/material/TextField'
+import frLocale from 'date-fns/locale/fr'
+import enLocale from 'date-fns/locale/en-US'
 import { convertDateSingleDay } from '../lib/utils'
 
 const DateSelector = ({ interval }) => {
   const [value, setValue] = useState(null)
   const maxDate = convertDateSingleDay(new Date())
 
+  const pickLocale = () => {
+    const browserLocale = window.navigator.language
+    if (browserLocale === 'fr') {
+      return frLocale
+    }
+    return enLocale
+  }
+
   return (
     <>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalizationProvider dateAdapter={AdapterDateFns} locale={pickLocale()}>
         <DatePicker
           views={[interval]}
           label="Date"
