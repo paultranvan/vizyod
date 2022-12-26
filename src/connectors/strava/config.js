5cd95c340510dbe3053ab03aabf29cef3aa2e19f
Paul Tran-Van <paul@cozycloud.cc>
2022-12-27 00:48:30 +0100

    refactor: Running is now activity

--- src/connectors/strava/config.js
+++ src/connectors/strava/config.js
@@ -4,6 +4,6 @@ module.exports = {
   TOKEN_URL: 'https://www.strava.com/oauth/token',
   SCOPE: ['activity:read_all', 'profile:read_all'],
   DATA_TYPES: {
-    ACTIVITY: 'running', // FIXME: there should be unified data type shared between conenctors with same data format 
+    ACTIVITY: 'activity', 
   }
 }
